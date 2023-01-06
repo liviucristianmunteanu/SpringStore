ebadad8b2e3e21810c3dd052c848173f76fbc301
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-01-06 09:41:20 +0200

    First commit

---
+++ src/test/java/com/myDomain/SpringStore/SpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myDomain.SpringStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
