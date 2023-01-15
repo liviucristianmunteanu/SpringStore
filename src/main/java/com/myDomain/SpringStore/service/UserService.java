e15a3bb8b8ec58c159d0d8128fc048ea179c80c6
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-01-15 20:08:18 +0200

    created User Service

---
+++ src/main/java/com/myDomain/SpringStore/service/UserService.java
@@ -0,0 +1,24 @@
+package com.myDomain.SpringStore.service;
+
+import com.myDomain.SpringStore.model.MyUser;
+import com.myDomain.SpringStore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<MyUser> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<MyUser> findById(Integer id) {
+        return userRepository.findById(id);
+    }
+}
